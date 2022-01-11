de425c62116f93c584baa7dbbfd4a50a2ed857e0
Tiago Aguiar <tyago_tka48@hotmail.com>
2022-01-11 10:18:17 -0300

    style: ajusta tamanho dos paineis na tela hero

--- src/pages/Hero/styled.ts
+++ src/pages/Hero/styled.ts
@@ -14,6 +14,14 @@ export const Content = styled.div`
   .panel {
     padding: 10px;
     border: 1px solid #999;
+    border-radius: 2px;
     margin: 10px;
+    box-shadow: 2px 2px 3px rgba(0,0,0,.28);
+    width: 40%;
+
+    h2 {
+      text-align: center;
+      text-transform: uppercase;
+    }
   }
 `;
